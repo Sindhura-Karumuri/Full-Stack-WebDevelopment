3c4a977c06c66a010b59ebf736072ed707e87fe0
Mohana Sri Lakshmi Sindhura Karumuri <karu07sind@gmail.com>
2025-02-03 21:01:43 +0530

    Add files via upload

---
+++ day5/JS-1/index.js
@@ -0,0 +1,34 @@
+console.log("Hello world");
+console.log('Hello world')
+//creative variables
+let a;
+console.log("datatype is ",typeof a);
+console.log("value of a is ",a);
+
+a=10;
+console.log("datatype is ",typeof a);
+console.log("value of a is ",a);
+
+a='hi'; //a = "hello"
+console.log("datatype is ",typeof a);
+console.log("value of a is ",a);
+
+a=true;
+console.log("datatype is ",typeof a);
+console.log("value of a is ",a);
+
+a=[]
+console.log("datatype is ",typeof a);
+console.log("value of a is ",a);
+
+//array
+let marks = [100,99,98]
+let names = ['sinu','ravi']
+
+//object
+let person = {
+    pid:100, //property = (key, value)
+    name: 'Sinu',
+    age : 20,
+    email: 'Sinu@gmail.com',
+}
